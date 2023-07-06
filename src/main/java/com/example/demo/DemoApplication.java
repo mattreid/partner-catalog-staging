51f570578a2886f6760f1ce345592626effe6295
Burr Sutter <burrsutter@gmail.com>
2023-07-06 10:22:43 -0400

    2x

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,8 +41,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("2: " + cnt);
-      return "2 Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("2x: " + cnt);
+      return "2x Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
